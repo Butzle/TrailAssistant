572cc5f8f5831d3d4847bf8621586283418d574b
Jo commit <jo.jos@hotmail.de>
2015-11-10 16:18:49 +0100

    Added Button

--- app/src/main/java/lu/uni/trailassistant/TrainingInstruction.java
+++ app/src/main/java/lu/uni/trailassistant/TrainingInstruction.java
@@ -2,6 +2,7 @@ package lu.uni.trailassistant;
 
 /**
  * Created by GOMES Leandro on 07.11.15.
+ *
  */
 public class TrainingInstruction {
     public enum TrainingMode {SPRINT, WALK, NORMAL, STRETCH}
