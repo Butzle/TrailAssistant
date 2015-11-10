fe5c99e67786937fb6d9bcfa38da216c142c34d0
Jo commit <jo.jos@hotmail.de>
2015-11-10 15:56:47 +0100

    Initial commit

---
+++ app/src/main/java/lu/uni/trailassistant/TrainingInstruction.java
@@ -0,0 +1,12 @@
+package lu.uni.trailassistant;
+
+/**
+ * Created by GOMES Leandro on 07.11.15.
+ */
+public class TrainingInstruction {
+    public enum TrainingMode {SPRINT, WALK, NORMAL, STRETCH}
+
+    private int distance;
+    private int duration = 0;
+    private TrainingMode trainingMode;
+}
